badcc55fdc2fe10b07822a7160db30a36972a912
choiseognjun <csj2702@naver.com>
2020-07-08 17:46:55 +0900

    IP변경

--- src/lib/api/client.js
+++ src/lib/api/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 //http://172.30.1.44:8080
-const client = axios.create({ baseURL: "http://localhost:8080" });
+const client = axios.create({ baseURL: "http://52.79.57.173" });
 
 export default client;
